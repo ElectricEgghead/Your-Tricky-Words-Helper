9c9603c3ef7d7f91068c9d614edd674b8cc28318
Ashleigh Bonner <14833914+ElectricEgghead@users.noreply.github.com>
2020-02-13 04:22:36 -0500

    Update main.cpp

--- Code/Original Code/main.cpp
+++ Code/Original Code/main.cpp
@@ -1,1 +1,242 @@
-placeholder
+/** Programmer: AB
+ * Date: 1/6/2020
+ * Description: Application that takes in a letter and displays tricky words
+ * that are often misspelled.
+ * 
+ * */
+
+#include <iostream>
+
+#include <string>
+
+#define string std::string // in IDE, #define statements will be in the usingDeclarations.h file
+
+#define cout std::cout
+
+#define cin std::cin
+
+#define endl std::endl
+
+
+char userLetterInput;
+
+string userFirstName;
+
+string userChoiceContinue;
+
+string pause;
+
+
+int main(){   
+
+    cout << "-----------<>-------------\n";
+
+    cout << "Welcome to your TRICKY WORDS Helper!\n";
+
+    cout << "\n*NOTE: All of the words may not be in alphabetical order.\n";
+
+    cout << "-----------<>--------------";
+
+    cout << "\n\nEnter your first name: ";
+
+    cin >> userFirstName;
+
+   
+    do {
+
+        cout << "Enter a lowercase letter [a - z]: ";
+
+        cin >> userLetterInput;
+
+        cout << "\n\n" <<userFirstName << ", " << "\n\nHere's your list"
+
+        " of tricky words for the letter (" << userLetterInput << "):";
+     
+     switch(userLetterInput) {
+
+            case 'a' :
+
+            cout << "\n\na while\nabsence\nacross\naccelerate \nacceptable\naccidentally\naccommodate\naccomplish\naccordion\naccumulate\nacknowledge\nacquaintance\nacquire\nadvice\naficionado\nacquit\nall right (NOT alright)\namateur\nanoint\napology\napparent\narctic\nargument\natheist\naxle"; 
+
+            break;
+
+            case 'b' :
+
+            cout << "\n\nbarbecue\nbeginning\nbelieve\nbellwether\nbicycle\nbroccoli\nbureau\nbusiness"; 
+
+            break;
+
+            case 'c' :
+
+            cout << "\n\ncalendar\ncamaraderie\ncamouflage\ncantaloupe\ncarburetor \nCaribbean\ncartilage\ncategory\nceiling\ncemetery\nchangeable\nchauvinism\nchili = hot pepper\nchilly = cold outside\nChile = a country\nchocolaty\ncoliseum \ncollectible\ncolumn\ncommemorate\ncommitted\nConnecticut\ncongratulations\nconscience\nconscientious\nconscious\nconsensus\nconvalesce\ncoolly\ncriticize"; 
+
+            break;
+
+            case 'd' :
+
+            cout << "\n\ndaiquiri\ndiscipline\ndeductible \ndefinite(ly)\ndrunkenness\ndumbbell\ndalmatian\ndecease\ndeceive\ndefendant\ndefiant\ndescent\ndesiccate\ndesperate\ndeterrence\ndevelopment\ndevice\ndiorama\ndisappear\ndisappoint\ndisastrous\ndissipate\ndifference\ndying = death is coming\ndieing = change hair color"; 
+
+            break;
+
+            case 'e' :
+
+            cout << "\n\necstasy\nembarrass(ment)\nenough\nespecially\nequipment\nexhilarate\nexceed\nexcellent\nexercise\nexistence\nexperience\nexplanation"; 
+
+            break;
+
+            case 'f' :
+
+            cout << "\n\nfahrenheit\nfascinate\nfiery\nfinally\nforeign\nFebruary\nflabbergast\nflotation\nfluorescent\nfourth\nfulfill"; 
+
+            break;
+
+            case 'g' :
+
+            cout << "\n\ngauge\ngenerally\ngenius\ngovernment\ngrammar\ngrateful\ngross\nguarantee\nguttural";
+
+            break;
+
+            case 'h' :
+
+            cout << "\n\nhandkerchief\nharass\nheight\nhierarchy\nhorrific\nhumorous\nhypocrisy"; 
+
+            break;
+
+            case 'i' :
+
+            cout << "\n\nignorance\nindict \nimmediate\nindependent\nindispensable\ninoculate\nintelligence\nIts (belongs to it)\nIt's (it is)\nimitate\ninadvertent\nincidentally\nincredible\ningenious\nirascible\nirresistible"; 
+
+            break;
+
+            case 'j' :
+
+            cout << "\n\njealous\njewelry\njudgment"; 
+
+            break;
+
+            case 'k' :
+
+            cout << "\n\nketchup\nkernel = piece of corn or a small amount\ncolonel = high-ranking officer\nknowledge"; 
+
+            break;
+
+            case 'l' :
+
+            cout << "\n\nlabeled\nled \nleisure\nliaison\nlibrary\nlicense\nlieutenant\nliquefy\nlose = lose a game\nloose = baggy clothes\nlying";
+
+            break;
+
+            case 'm' :
+
+            cout << "\n\nmagically\nmaintenance\nmarshmallow \nmathematics\nmaneuver\nmedieval\nmediocre\nmemento\nmillennium\nminiature \nminuscule\nmischief\nmischievous\nmiscellaneous\nmisogyny\nmisspell (mis + spell)\nmysterious";
+
+            break;
+
+            case 'n' :
+
+            cout << "\n\nnauseous\nnecessary (cess)\nneighbor\nno one (but NOBODY is one word)\nnoticeable\nnuclear";
+
+            break;
+
+            case 'o' :
+
+            cout << "\n\noccasionally\noccur\noccurred\noccurrence\noctopus\nodyssey\nofficial\nonomatopoeia\n"; 
+
+            break;
+
+            case 'p' :
+
+            cout << "\n\npastime\nperseverance\npersonnel\npharaoh\nplaywright \npossession\nprecede\nprejudice\nprincipal = (school PAL)\nprinciple (a rule)\nprivilege\npronunciation \npublicly\nparallel\nparliament\nparticular\npeninsula\npiece\npigeon\npistachio\npleasant\nplenitude\npreferable\npresumptuous\nproceed\npropagate\npuerile\npumpkin\npursue\nputrefy"; 
+
+            break;
+
+            case 'q' :
+
+            cout << "\n\nquestionnaire"; 
+
+            break;
+
+            case 'r' :
+
+            cout << "\n\nraspberry \nreceive\nreceipt\nrecommend\nreferred\nreference\nrefrigerator\nrelevant\nreligious\nremembrance \nrenowned\nrestaurant\nrhyme\nridiculous"; 
+
+            break;
+
+            case 's' :
+
+            cout << "\n\nschedule\nseparate\nsergeant\nsupersede\nsacrilegious\nsalary\nsandal\nsandwich\nsavvy\nscience\nscissors\nseize\nsensible\nseparate\nseptuagenarian\nsheriff\nshish kebab\nsiege\nsimilar\nsincerely\nspecial\nsubpoena\nsuccess\nsimile"; 
+
+            break;
+
+            case 't' :
+
+            cout << "\n\ntableau\ntariff\ntheir = belongs to them\nthey're = they are\nthere = somewhere over there\nthorough\nthreshold\nthrough\ntomorrow\ntongue\ntoo = 'I am great, as well'\nto = 'go to the store'\ntwo = '2'\ntragedy \ntruly\ntwelfth\ntyranny"; 
+
+            break;
+
+            case 'u' :
+
+            cout << "\n\nuntil\nukulele\nusage";
+
+            break;
+
+            case 'v' :
+
+            cout << "\n\nvacuum";
+
+            break;
+
+            case 'w' :
+
+            cout << "\n\nweather\nWednesday\nweird"; 
+
+            break;
+
+            case 'x' :
+
+            cout << "\n\n*Words to be added.*"; 
+
+            break;
+
+            case 'y' :
+
+            cout << "\n\n*Words to be added.*"; 
+
+            break;
+
+            case 'z' :
+
+            cout << "\n\n*Words to be added.*";  
+
+            break;
+
+      default :
+
+         cout << "\n\nERROR: Invalid input. Please enter a lowercase"
+
+         " letter [a -z]." << endl;
+
+   } // end switch   
+
+   cout << "\n\nWould you like to enter another letter [Y, N]?: ";
+
+   cin >> userChoiceContinue;
+
+    } // end do
+
+   while (userChoiceContinue == "y" | userChoiceContinue == "Y"); 
+
+   
+ cout << "\n\n-------------------------";
+
+ cout << "\nThank you for using TRICKY WORDS Helper. Good luck and happy spelling!\n";
+
+ cout << "-------------------------\n";
+
+ cout << "\nPress [E then ENTER] to end TRICKY...";
+
+ cin >> pause;
+  
+ return 0;
+
+    }
