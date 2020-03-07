e82ebe6c1bdebc9a1353278433050784cac3e84c
Ashleigh Bonner <14833914+ElectricEgghead@users.noreply.github.com>
2020-03-07 04:49:45 -0500

    Create usingDeclarations.h

---
+++ Code/usingDeclarations.h
@@ -0,0 +1,8 @@
+#define cout std::cout
+#define cin std::cin
+#define getline std::getline
+#define endl std::endl
+#define string std::string
+#define ifstream std::ifstream
+#define cerr std::cerr
+#define vector std::vector
