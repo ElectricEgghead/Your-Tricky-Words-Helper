30ba09347e1b3e956321594d44f56c282bad594d
Ashleigh Bonner <14833914+ElectricEgghead@users.noreply.github.com>
2020-03-14 08:19:49 -0400

    Update usingDeclarations.h

--- Code/usingDeclarations.h
+++ Code/usingDeclarations.h
@@ -2,10 +2,13 @@
 
 using std::cout;
 using std::cin;
-using std::getline;
-using std::endl;
 using std::cerr;
 
+using std::getline;
+using std::endl;
+
+using std::numeric_limits;
+using std::streamsize;
 using std::string;
 using std::ifstream;
 using std::vector;
